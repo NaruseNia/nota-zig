865800af8636306e57fb10874b278da28d334470
Naruse Nia <notshinsin0000@gmail.com>
2026-06-05 15:42:59 +0900

    add(parser): parse char class

--- src/regex/ast.zig
+++ src/regex/ast.zig
@@ -3,14 +3,43 @@ pub const CharClass = struct {
     negated: bool,
 };
 
+pub fn charClass(ranges: []const Range, negated: bool) CharClass {
+    return CharClass{
+        .ranges = ranges,
+        .negated = negated,
+    };
+}
+
 pub const Group = struct {
     child: *const Node,
 };
+
+pub fn group(child: *const Node) Group {
+    return Group{ .child = child };
+}
+
+pub const Alt = struct {
+    options: []const Node,
+};
+
+pub fn alt(options: []const Node) Alt {
+    if (options.len < 2) {
+        @compileError("Alternation requires at least two options");
+    }
+    return Alt{ .options = options };
+}
 
 pub const Range = struct {
     start: u8,
     end: u8,
 };
+
+pub fn range(start: u8, end: u8) Range {
+    if (start > end) {
+        @compileError("Invalid range: start must be less than or equal to end");
+    }
+    return Range{ .start = start, .end = end };
+}
 
 pub const Repeat = struct {
     child: *const Node,
@@ -19,13 +48,52 @@ pub const Repeat = struct {
     max: ?usize,
 };
 
+pub fn repeat(child: *const Node, min: usize, max: ?usize) Repeat {
+    if (max != null and min > max.?) {
+        @compileError("Invalid repetition: min must be less than or equal to max");
+    }
+    return Repeat{
+        .child = child,
+        .min = min,
+        .max = max,
+    };
+}
+
 /// A regex AST node
 pub const Node = union(enum) {
     literal: u8,
     dot,
     concat: []const Node,
-    alt: []const Node,
+    alt: Alt,
     char_class: CharClass,
     group: Group,
     repeat: Repeat,
+
+    pub fn literalNode(c: u8) Node {
+        return Node{ .literal = c };
+    }
+
+    pub fn dotNode() Node {
+        return Node{ .dot = {} };
+    }
+
+    pub fn concatNodes(nodes: []const Node) Node {
+        return Node{ .concat = nodes };
+    }
+
+    pub fn altNodes(nodes: []const Node) Node {
+        return Node{ .alt = alt(nodes) };
+    }
+
+    pub fn charClassNode(ranges: []const Range, negated: bool) Node {
+        return Node{ .char_class = charClass(ranges, negated) };
+    }
+
+    pub fn groupNode(child: *const Node) Node {
+        return Node{ .group = group(child) };
+    }
+
+    pub fn repeatNode(child: *const Node, min: usize, max: ?usize) Node {
+        return Node{ .repeat = repeat(child, min, max) };
+    }
 };
--- src/regex/parser.zig
+++ src/regex/parser.zig
@@ -1,28 +1,94 @@
 const std = @import("std");
 const ast = @import("ast.zig");
 
-const State = struct {
+const Cursor = struct {
+    const Self = @This();
+
     input: []const u8,
     curr: usize,
+
+    /// Gets the current character without advancing the cursor.
+    /// If the cursor is out of bounds, it will cause a compile error.
+    fn currChar(self: *const Self) u8 {
+        if (self.isOutOfBounds()) {
+            @compileError("Unexpected end of pattern");
+        }
+        return self.input[self.curr];
+    }
+
+    /// Peeks ahead by `amount` characters without advancing the cursor.
+    fn peek(self: *const Self, amount: u8) ?u8 {
+        if (self.curr + amount < self.input.len) {
+            return self.input[self.curr + amount];
+        }
+
+        return null;
+    }
+
+    /// Peeks the next character without advancing the cursor.
+    fn peekNext(self: *const Self) ?u8 {
+        if (self.curr + 1 < self.input.len) {
+            return self.input[self.curr + 1];
+        }
+        return null;
+    }
+
+    /// Takes the next `amount` characters and advances the cursor.
+    fn take(self: *Self, amount: usize) []const u8 {
+        if (self.curr + amount > self.input.len) {
+            @compileError("Unexpected end of pattern");
+        }
+        const slice = self.input[self.curr .. self.curr + amount];
+        self.curr += amount;
+
+        return slice;
+    }
+
+    /// Takes the next character and advances the cursor.
+    fn takeNext(self: *Self) u8 {
+        if (self.curr >= self.input.len) {
+            @compileError("Unexpected end of pattern");
+        }
+        const c = self.input[self.curr];
+        self.curr += 1;
+        return c;
+    }
+
+    /// Increments the cursor by 1 without returning the current character.
+    fn skip(self: *Self) void {
+        self.curr += 1;
+    }
+
+    /// Checks if the cursor is out of bounds (i.e. has reached the end of the input).
+    fn isOutOfBounds(self: *const Self) bool {
+        return self.curr >= self.input.len;
+    }
+
+    /// Checks if the cursor is in bounds (i.e. has not reached the end of the input).
+    fn isInBounds(self: *const Self) bool {
+        return self.curr < self.input.len;
+    }
 };
 
+/// Parses a regex pattern at compile time and returns an `ast.Node` representing the parsed pattern.
 pub fn parse(comptime pattern: []const u8) ast.Node {
-    comptime var state = State{
+    comptime var state = Cursor{
         .input = pattern,
         .curr = 0,
     };
     comptime var nodes: []const ast.Node = &.{};
     // Reading each character of the `pattern`
-    while (state.curr < state.input.len) {
-        const c = state.input[state.curr];
-        state.curr += 1;
+    while (state.isInBounds()) {
+        const c = state.takeNext();
 
         switch (c) {
             // Escape sequences
             '\\' => nodes = nodes ++ &[_]ast.Node{parseEscaped(&state)},
+            // Char class
+            '[' => nodes = nodes ++ &[_]ast.Node{parseCharClass(&state)},
             // Literals
             else => {
-                nodes = nodes ++ &[_]ast.Node{.{ .literal = c }};
+                nodes = nodes ++ &[_]ast.Node{ast.Node.literalNode(c)};
             },
         }
     }
@@ -30,47 +96,83 @@ pub fn parse(comptime pattern: []const u8) ast.Node {
     return switch (nodes.len) {
         0 => @compileError("Empty pattern is not supported"),
         1 => nodes[0],
-        else => ast.Node{ .concat = nodes },
+        else => ast.Node.concatNodes(nodes),
     };
 }
 
-fn parseEscaped(comptime state: *State) ast.Node {
-    if (state.curr >= state.input.len) {
+/// Parses an escape sequence (e.g. \n, \t, \w) and returns an `ast.Node` representing it.
+fn parseEscaped(comptime cursor: *Cursor) ast.Node {
+    if (cursor.isOutOfBounds()) {
         @compileError("Unexpected end of pattern after '\\'");
     }
-    const next = state.input[state.curr];
-    state.curr += 1;
+
+    const next = cursor.takeNext();
+
     switch (next) {
-        't' => return ast.Node{ .literal = '\t' },
-        'n' => return ast.Node{ .literal = '\n' },
-        'r' => return ast.Node{ .literal = '\r' },
-        '\\' => return ast.Node{ .literal = '\\' },
-        'w' => return ast.Node{ .char_class = ast.CharClass{
-            .ranges = &[_]ast.Range{
-                .{ .start = 'a', .end = 'z' },
-                .{ .start = 'A', .end = 'Z' },
-                .{ .start = '0', .end = '9' },
-                .{ .start = '_', .end = '_' },
-            },
-            .negated = false,
-        } },
-        'd' => return ast.Node{ .char_class = ast.CharClass{
-            .ranges = &[_]ast.Range{
-                .{ .start = '0', .end = '9' },
-            },
-            .negated = false,
-        } },
-        's' => return ast.Node{ .char_class = ast.CharClass{
-            .ranges = &[_]ast.Range{
-                .{ .start = ' ', .end = ' ' },
-                .{ .start = '\t', .end = '\t' },
-                .{ .start = '\n', .end = '\n' },
-                .{ .start = '\r', .end = '\r' },
-            },
-            .negated = false,
-        } },
+        't' => return ast.Node.literalNode('\t'),
+        'n' => return ast.Node.literalNode('\n'),
+        'r' => return ast.Node.literalNode('\r'),
+        '\\' => return ast.Node.literalNode('\\'),
+        'w' => return ast.Node.charClassNode(&[_]ast.Range{
+            ast.range('a', 'z'),
+            ast.range('A', 'Z'),
+            ast.range('0', '9'),
+            ast.range('_', '_'),
+        }, false),
+        'd' => return ast.Node.charClassNode(&[_]ast.Range{
+            ast.range('0', '9'),
+        }, false),
+        's' => return ast.Node.charClassNode(&[_]ast.Range{
+            ast.range(' ', ' '),
+            ast.range('\t', '\t'),
+            ast.range('\n', '\n'),
+            ast.range('\r', '\r'),
+        }, false),
         else => @compileError("Unsupported escape sequence"),
     }
+}
+
+/// Parses a character class (e.g. [abc], [a-z], [^0-9]) and returns an `ast.Node` representing it.
+fn parseCharClass(comptime cursor: *Cursor) ast.Node {
+    if (cursor.isOutOfBounds()) {
+        @compileError("Unexpected end of pattern in character class");
+    }
+
+    comptime var ranges: []const ast.Range = &.{};
+
+    comptime var negated = false;
+    if (cursor.isInBounds() and cursor.currChar() == '^') {
+        negated = true;
+        cursor.skip();
+    }
+
+    // Read until closing ']'
+    while (cursor.isInBounds()) {
+        if (cursor.currChar() == ']') {
+            cursor.skip();
+            break;
+        }
+
+        // Handle ranges (e.g. a-z) and escape sequences
+        const next = cursor.takeNext();
+        if (cursor.isOutOfBounds()) {
+            @compileError("Unexpected end of pattern in character class");
+        }
+
+        // Range (e.g. [a-z]) Peek next character to check for '-'
+        if (cursor.currChar() == '-') {
+            cursor.skip(); // Skip '-'
+            const end = cursor.takeNext();
+
+            // Add range to char class
+            ranges = ranges ++ &[_]ast.Range{ast.range(next, end)};
+        } else {
+            // Literals (e.g [abc])
+            ranges = ranges ++ &[_]ast.Range{ast.range(next, next)};
+        }
+    }
+
+    return ast.Node.charClassNode(ranges, negated);
 }
 
 test "parse single literal" {
@@ -95,4 +197,23 @@ test "parse \\w character class" {
     const node = comptime parse("\\w");
     try std.testing.expect(node == .char_class);
     try std.testing.expectEqual(4, node.char_class.ranges.len);
+}
+
+test "parse simple char class" {
+    const node = comptime parse("[abc]");
+    try std.testing.expect(node == .char_class);
+    try std.testing.expectEqual(3, node.char_class.ranges.len);
+}
+
+test "parse simple range char class" {
+    const node = comptime parse("[a-z]");
+    try std.testing.expect(node == .char_class);
+    try std.testing.expectEqual(1, node.char_class.ranges.len);
+}
+
+test "parse negated char class" {
+    const node = comptime parse("[^0-9]");
+    try std.testing.expect(node == .char_class);
+    try std.testing.expect(node.char_class.negated);
+    try std.testing.expectEqual(1, node.char_class.ranges.len);
 }
